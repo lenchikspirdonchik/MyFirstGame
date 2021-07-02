991aeadbb0a151b0118cad9e277d23698c3e7b15
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-07-02 11:22:39 +0300

    cool shit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,45 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public float force;
     public float jumpForse;
+    protected Joystick joystick;
+    protected JoyButton joyButton;
+    protected bool jump;
     void Start()
     {
-     
+        joystick = FindObjectOfType<Joystick>();
+        joyButton = FindObjectOfType<JoyButton>();
     }
 
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.W))
-            rb.AddForce(Vector3.forward * (force * Time.fixedDeltaTime));
-      
-        if (Input.GetKey(KeyCode.S))
-            rb.AddForce(Vector3.forward * (-force * Time.fixedDeltaTime));
-        
-        if (Input.GetKey(KeyCode.D))
-            rb.AddForce(Vector3.right * (force * Time.fixedDeltaTime));
-        
-        if (Input.GetKey(KeyCode.A))
-            rb.AddForce(Vector3.right * (-force * Time.fixedDeltaTime));
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(Vector3.up * (jumpForse * Time.fixedDeltaTime));
+ 
+     void FixedUpdate()
+     {
+         rb.velocity = new Vector3(joystick.Horizontal * 10f,
+             rb.velocity.y,
+             joystick.Vertical * 10f);
+         if (Input.GetKey(KeyCode.W))
+             rb.AddForce(Vector3.forward * (force * Time.fixedDeltaTime));
        
-    }
+         if (Input.GetKey(KeyCode.S))
+             rb.AddForce(Vector3.forward * (-force * Time.fixedDeltaTime));
+         
+         if (Input.GetKey(KeyCode.D))
+             rb.AddForce(Vector3.right * (force * Time.fixedDeltaTime));
+         
+         if (Input.GetKey(KeyCode.A))
+             rb.AddForce(Vector3.right * (-force * Time.fixedDeltaTime));
+         
+         if (Input.GetKeyDown(KeyCode.Space))
+             rb.AddForce(Vector3.up * (jumpForse * Time.fixedDeltaTime));
+
+         if (!jump && joyButton.isPressed)
+         {
+             jump = true;
+             rb.velocity = Vector3.up * 10f;
+         }
+
+         if (jump && !joyButton.isPressed)
+         {
+             jump = false;
+         }
+     }
 }
